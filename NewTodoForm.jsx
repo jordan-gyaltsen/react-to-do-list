70ef0924f65b6409ee1d3b741c9e4f64b570d908
Jordan Gyaltsen <jordan-gyaltsen@users.noreply.github.com>
2023-07-08 20:52:54 -0400

    Add files via upload

---
+++ NewTodoForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export function NewTodoForm({ onSubmit }) {
+  const [newItem, setNewItem] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newItem === "") return;
+
+    onSubmit(newItem);
+    setNewItem("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="newitemform">
+      <div className="formrow">
+        <label htmlFor="item">New Item</label>
+        <input value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+          type="text" id="item" />
+      </div>
+      <h1>{newItem}</h1>
+      <button className="btn">Add</button>
+    </form>
+  )
+}
